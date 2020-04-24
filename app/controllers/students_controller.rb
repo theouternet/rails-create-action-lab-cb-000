17c243c1906afd3b09aa8bf169339c78fa351fa7
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-24 06:56:54 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-  end
-
+   @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student)
+end 
 end
