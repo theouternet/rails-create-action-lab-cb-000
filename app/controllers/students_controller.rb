9fc094cba03f4ad2cd13efa13b066925bea0ddaf
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-24 06:12:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create()
+    @student = Student.create(first_name params[:first_name], )
   end
 
 end
