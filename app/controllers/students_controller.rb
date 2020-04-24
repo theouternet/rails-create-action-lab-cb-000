22f25487a388ba16afee99fd783b327f3464efe2
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-24 06:51:56 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,8 @@ class StudentsController < ApplicationController
 
   def new
   end
+  
+  def create
 
    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to student_path(@student)
